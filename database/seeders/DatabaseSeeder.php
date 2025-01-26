f382aa4c75c692bb2494c5cceb04b89e355c59b6
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-26 09:38:50 +0100

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Modules\Acl\Database\Seeders\AclPermissionSeeder;
 use Modules\Acl\Database\Seeders\AclRoleSeeder;
+use Modules\Blog\Database\Seeders\BlogAclSeeder;
+use Modules\Blog\Database\Seeders\BlogSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,7 @@ class DatabaseSeeder extends Seeder
             AclRoleSeeder::class,
             AclPermissionSeeder::class,
         ]);
+        $this->call(BlogAclSeeder::class);
+    $this->call(BlogSeeder::class);
     }
 }
